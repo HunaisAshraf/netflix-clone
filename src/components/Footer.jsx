9a6cb49087fc0fb094cb9df09180606445fd1ffd
HunaisAshraf <hunaisashraf@gmail.com>
2024-03-21 12:03:23 +0530

    completed header & banner

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,16 @@ import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
 const Footer = () => {
   return (
     <div className="flex items-center justify-around">
-      <div className="flex ">
+      <div>
         <ul className="flex text-2xl gap-4">
           <li><FaFacebookF /></li>
           <li><FaInstagram /></li>
           <li><FaTwitter /></li>
           <li><FaYoutube /></li>
         </ul>
+      </div>
+      <div className="flex">
+         
       </div>
     </div>
   );
